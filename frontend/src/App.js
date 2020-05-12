fb8a79c5c9c743f2433d969f673931b0eec5225a
Ryan Sutrisno <ryan.sutrisno@myinfosys.net>
2020-05-12 13:55:34 +0700

    create backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   const openMenu = () => {
@@ -10,131 +12,43 @@ function App() {
     document.querySelector('.sidebar').classList.remove('open');
   };
   return (
-    <div className='grid-container'>
-      <header className='header'>
-        <div className='brand'>
-          <button onClick={openMenu}>&#9776;</button>
-          <a href='index.html'>amazonk</a>
-        </div>
-        <div className='header-links'>
-          <a href='cart.html'>Cart</a>
-          <a href='signin.html'>Sign In</a>
-        </div>
-      </header>
-      <aside className='sidebar'>
-        <h3>Shopping Category</h3>
-        <button className='sidebar-close-button' onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href='index.html'>Pants</a>
-          </li>
-          <li>
-            <a href='index.html'>Shirt</a>
-          </li>
-        </ul>
-      </aside>
-      <main className='main'>
-        <div className='content'>
-          <ul className='products'>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">amazonk</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Category</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
             <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
+              <a href="index.html">Pants</a>
             </li>
             <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className='product'>
-                <img
-                  className='product-image'
-                  src='./images/d1.jpeg'
-                  alt='product'
-                />
-                <div className='product-name'>
-                  <a href='product.html'>Slim Shirt</a>
-                </div>
-                <div className='product-brand'>Nike</div>
-                <div className='product-price'>$60</div>
-                <div className='product-rating'>$4.5 stars (10 Reviews)</div>
-              </div>
+              <a href="index.html">Shirt</a>
             </li>
           </ul>
-        </div>
-      </main>
-      <footer className='footer'>
-        Copyright &copy; 2020. All right Reserved.
-      </footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">
+          Copyright &copy; 2020. All right Reserved.
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirt',
+      image: '/images/d1.jpeg',
+      brand: 'Nike',
+      price: 60,
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirt',
+      image: '/images/d1.jpeg',
+      brand: 'Nike',
+      price: 65,
+      rating: 4.2,
+      numReviews: 7,
+    },
+    {
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpeg',
+      brand: 'Nike',
+      price: 70,
+      rating: 4.5,
+      numReviews: 5,
+    },
+  ],
+};
